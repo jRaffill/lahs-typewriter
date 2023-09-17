be742775819bf87b7ef3aa79e4bfe8ec23558c51
jRaffill <jayalakshmi@damajash.org>
2023-09-16 21:30:50 -0700

    Very basic template for post pages!
    * Still just fake text, but now the link to posts/intro.html actually goes somewhere :O
    * Needs some aesthetic improvements (like colors other than black and white for example)
    * Once we have more posts, I'll want to add some sort of "similar posts" links
      * This'll also actually utilize postScript.js!
    * Add some sort of minimized banner that sticks to the top of the page throughout?

---
+++ posts/postScript.js
@@ -0,0 +1,5 @@
+import {allTags} from "../posts.js"
+var tagsContainer = document.getElementById("tags")
+for (const i of allTags) {
+	tagsContainer.innerHTML += "<a class=\"tag\" href=\"tags/" + i + ".html\">" + i + "</a>";
+}
