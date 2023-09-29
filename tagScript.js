7a1ce15d419ea858b40960bd8ed7da02b4edd0c1
jRaffill <jayalakshmi@damajash.org>
2023-09-29 10:53:02 -0700

    Add newsletter signup (functionality)
    * You can sign up to the newsletter now!
    * Saves to a google sheet on my school email for now -- may want to change it to save to the club email instead

--- tagScript.js
+++ tagScript.js
@@ -34,3 +34,19 @@ var tagsContainer = document.getElementById("tags")
 for (const tag of allTags) {
 	tagsContainer.innerHTML += "<a class=\"tag\"href=\"tag.html?tag=" + tag + "\">" + tag + "</a>";
 }
+
+const form = document.getElementById("form");
+form.addEventListener("submit", function(e) {
+	e.preventDefault();
+	const data = new FormData(form);
+	document.getElementById("subscribe").disabled = "true";
+	document.getElementById("email").disabled = "true";
+	const action = e.target.action;
+	fetch(action, {method: "POST", body: data}).then((response) => {
+		console.log(response.text());
+		form.innerHTML = "<p> Thank you for subscribing to our newsletter! </p>";
+	}).catch(response => {
+		console.error(response.text());
+		form.innerHTML = "<p> There was an error submitting your data. Please refresh the page and try again. </p>";
+	})
+})
