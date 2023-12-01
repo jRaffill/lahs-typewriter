5bde975d63f8dfdf117d8d5c5ae52fd157f5c2ef
jRaffill <jayalakshmi@damajash.org>
2023-11-30 18:48:29 -0800

    Add the first posts!
    We finally have enough submissions to release our first posts to the world! Tbf most of them are from our club officers but still! My first actually useful coding project! Pretty exciting :)

--- posts.js
+++ posts.js
@@ -1,31 +1,40 @@
 export const posts = [
-	{
-		"date": [2023, 9, 16],
-		"dateText": "Sept. 16, 2023",
-		"tags": ["news", "nonfiction"],
-		"author": "CWC staff",
-		"title": "We Have A Blog",
-		"url": "intro.html",
-		"contentPreview": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci a scelerisque purus semper eget duis at. Feugiat scelerisque varius morbi enim nunc faucibus a pellentesque. Ipsum a arcu cursus vitae congue mauris rhoncus aenean. Odio tempor orci dapibus ultrices in iaculis. Vitae suscipit tellus mauris a diam. Nibh praesent tristique magna sit amet purus gravida quis blandit. Curabitur gravida arcu ac tortor. Quisque id diam vel quam elementum pulvinar etiam. Ultrices dui sapien eget mi. Sed vulputate mi sit amet mauris commodo quis. Quam nulla porttitor massa id neque aliquam vestibulum morbi. Elementum facilisis leo vel fringilla est ullamcorper eget nulla facilisi. Felis imperdiet proin fermentum leo vel orci. Tortor pretium viverra suspendisse potenti. Nunc pulvinar sapien et ligula ullamcorper malesuada proin libero."
-	},
-	{
-		"date": [2023, 9, 15],
-		"dateText": "Sept. 15, 2023",
-		"tags": ["writing-advice", "nonfiction"],
-		"author": "CWC staff",
-		"title": "alsdjflajdfioarghe",
-		"url": "intro.html",
-		"contentPreview": "Sit amet porttitor eget dolor morbi. Lectus quam id leo in vitae turpis massa sed elementum. Tincidunt dui ut ornare lectus sit amet est placerat. Arcu non odio euismod lacinia at quis risus. Vitae congue mauris rhoncus aenean. Elementum tempus egestas sed sed. Orci sagittis eu volutpat odio facilisis. Purus viverra accumsan in nisl. Commodo viverra maecenas accumsan lacus vel facilisis. Ornare arcu odio ut sem nulla pharetra diam sit amet. Consequat ac felis donec et odio. Vulputate mi sit amet mauris commodo quis imperdiet massa tincidunt. Mauris a diam maecenas sed enim ut sem viverra aliquet. In nulla posuere sollicitudin aliquam ultrices. Sed lectus vestibulum mattis ullamcorper velit sed ullamcorper. Diam phasellus vestibulum lorem sed risus ultricies tristique. Sollicitudin nibh sit amet commodo nulla."
-	},
 		{
-		"date": [2023, 9, 15],
-		"dateText": "Sept. 15, 2023",
+		"date": [2023, 11, 30],
+		"dateText": "Nov. 30, 2023",
 		"tags": ["poetry"],
-		"author": "CWC staff",
-		"title": "oourhtwheroitq",
-		"url": "intro.html",
-		"contentPreview": "Viverra orci sagittis eu volutpat odio facilisis mauris sit. Et odio pellentesque diam volutpat commodo sed. Nisl nisi scelerisque eu ultrices. Enim neque volutpat ac tincidunt vitae semper quis lectus nulla. Integer vitae justo eget magna fermentum iaculis eu. Ipsum nunc aliquet bibendum enim facilisis gravida neque. A condimentum vitae sapien pellentesque habitant morbi tristique. Amet purus gravida quis blandit turpis. Phasellus egestas tellus rutrum tellus pellentesque. Massa tincidunt nunc pulvinar sapien et ligula. Auctor augue mauris augue neque gravida in fermentum et."
+		"author": "Laura Zhang, Senior",
+		"title": "Teenage Dream",
+		"url": "teenageDream.html",
+		"contentPreview": "Everything I’ve ever wanted fits / under one sparkly sunroof."
 	},
+	{
+		"date": [2023, 11, 28],
+		"dateText": "Nov. 28, 2023",
+		"tags": ["essay", "personal"],
+		"author": "Bridget Liu, Senior",
+		"title": "fruit tea",
+		"url": "fruitTea.html",
+		"contentPreview": "	I first learned to make fruit tea as a middle school student. My mother had just bought this grand contraption of a device—an “electric tea kettle,” apparently. It was innovative, all glass and stainless steel, with smoothed edges and round corners. My first sip of brewed grapefruit tea is carved into my brain vividly: a faint touch of bitterness incorporated into sweet splashes of citrus flavor."
+	},
+	{
+		"date": [2023, 10, 17],
+		"dateText": "Oct. 17, 2023",
+		"tags": ["short-story", "personal"],
+		"author": "Jayalakshmi Raffill, Sophomore",
+		"title": "Footsteps in the Snow",
+		"url": "footstepsInSnow.html",
+		"contentPreview": "My feet ache and my hands shiver slightly as I tug on my mom’s hand to bring her attention to a swirling snow globe. I’ve been watching the currents within it for the past few minutes and I’m entranced. Beneath the thick veil of white flakes my eyes trace the shape of a museum, and the silver calligraphy beneath that spells out, “MET.”"
+	},
+	{
+		"date": [2023, 10, 8],
+		"dateText": "Oct. 8, 2023",
+		"tags": ["poetry"],
+		"author": "Max Asmar, Freshman",
+		"title": "The Words You Say",
+		"url": "wordsYouSay.html",
+		"contentPreview": "The words you say to us impact our lives / The way we treat others, / The way we see the world."
+	}
 ];
 
-export const allTags = ["news", "nonfiction", "writing-advice", "poetry", "comedy"];
+export const allTags = ["poetry", "short-story", "personal", "essay"];
