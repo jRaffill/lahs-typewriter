e4a9f6ff434dc03fc11f64afe43a98e99964e7b7
jRaffill <jayalakshmi@damajash.org>
2024-05-13 18:22:59 -0700

    Add new short stories! <3

--- posts.js
+++ posts.js
@@ -1,14 +1,34 @@
 export const posts = [
+		{
+		"date": [2024, 4, 14],
+		"dateText": "Apr. 14, 2024",
+		"tags": ["short-story", "sci-fi"],
+		"author": "Michael Cheilek-Klaiber, Senior",
+		"title": "Times Lost",
+		"url": "timesLost.html",
+		"contentPreview": "Floating like a soap bubble in the wide expanse of the endless blue field of sky, the small airship lay still. The golden late-afternoon light flooded the deck, spilling onto the wind-grazed balcony. Past that warm glow that bathed the floor, in the dim interior of the ship barely touched by the waning light of the day, the captain sat regarding the timeworn papers spread out on his desk. The four manuscripts which were the key to the airship’s long travels he had masterminded."
+	},
+
+		{
+		"date": [2024, 3, 14],
+		"dateText": "Mar. 14, 2024",
+		"tags": ["short-story", "sci-fi"],
+		"author": "Michael Cheilek-Klaiber, Senior",
+		"title": "To Advertise For",
+		"url": "toAdvertiseFor.html",
+		"contentPreview": "“What?” I exclaimed to myself. <br/> It couldn’t be. I read the email again, and then for a third, fourth, time. It did not change what I was seeing."
+	},
 	{
 		"date": [2024, 2, 14],
 		"dateText": "Feb. 14, 2024",
-		"tags": ["short-story"],
+		"tags": ["short-story", "sci-fi"],
 		"author": "Michael Cheilek-Klaiber, Senior",
 		"title": "The Tomorrow of Our Dreams",
 		"url": "tomorrowOfDreams.html",
 		"contentPreview": "<em>Good morning, Blaine, </em> the voice in his head said as he jolted awake. <em>It is 3:30 AM. Your shift begins in 30 minutes. </em> Blaine struggled to open his eyes. 3:30 already? It seemed like he had just gone to sleep. He couldn’t believe that a good four hours of sleep had passed by just like that. But that’s how it always was."
 	},
 	{
+		
 		"date": [2023, 11, 30],
 		"dateText": "Nov. 30, 2023",
 		"tags": ["poetry"],
@@ -46,4 +66,4 @@ export const posts = [
 	}
 ];
 
-export const allTags = ["poetry", "short-story", "personal", "essay"];
+export const allTags = ["poetry", "short-story", "personal", "essay", "sci-fi"];
